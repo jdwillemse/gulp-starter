9cfb8339645587a17de0353afa054b3c81539013
JD Willemse <jd.willemse@akqa.com>
2015-07-23 16:47:14 +0200

    added local module example

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
+import {whaaaat} from './module';
+
 
 var message = '<h2>World</h2>';
 
 $('body').append(message);
 
-console.log('page.js loaded!');
+console.log('page.js loaded!', whaaaat());
---
+++ src/js/module.js
@@ -0,0 +1,3 @@
+export function whaaaat() {
+  return 'super magic';
+}
